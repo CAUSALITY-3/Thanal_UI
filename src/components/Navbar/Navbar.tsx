715e808879ffe4c6ed2527d471a3e25e0a295e64
CAUSALITY-3 <70097078+CAUSALITY-3@users.noreply.github.com>
2023-07-16 16:02:37 +0530

    navbar setup

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,44 @@
-import React, { FC } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
+import menu from '../../assets/menu.svg'
+import close from '../../assets/close.svg'
+import logo from '../../assets/logo.svg'
+import './Navbar.scss'
 
 const Navbar: FC = () => {
-    return (
-        <>
-            <NavLink to="/" >Home</NavLink>
-            <NavLink to="/Login" >Login</NavLink>
-            <NavLink to="/Product" >Product</NavLink>
-            <NavLink to="/Contact" >Contact Us</NavLink>
-        </>
-    )
+  const [toggleMenu, setToggleMenu] = useState(false)
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', changeWidth)
+
+    return () => {
+      window.removeEventListener('resize', changeWidth)
+    }
+
+  }, [])
+
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu)
+  }
+  return (
+    <nav>
+      <img id='Layer_1' src={logo} />
+      {(toggleMenu || screenWidth > 500) && (
+        <ul className="nav-list">
+          <NavLink className="nav-list-items" to="/Login" >Login</NavLink>
+          <NavLink className="nav-list-items" to="/Product" >Product</NavLink>
+          <NavLink className="nav-list-items" to="/Contact" >Contact Us</NavLink>
+        </ul>
+      )}
+      {toggleMenu ? (screenWidth <= 500) && <img onClick={toggleNav} className="menu" src={close} /> : (screenWidth <= 500) && <img onClick={toggleNav} className="menu" src={menu} />}
+    </nav>
+  )
 }
 
 export default Navbar
