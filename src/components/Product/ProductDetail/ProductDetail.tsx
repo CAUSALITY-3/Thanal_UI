177c17e91c2e5d420b470e6b2278a8ee7e0a3604
CAUSALITY-3 <70097078+CAUSALITY-3@users.noreply.github.com>
2023-09-29 12:59:15 +0530

    product detail card outline

--- src/components/Product/ProductDetail/ProductDetail.tsx
+++ src/components/Product/ProductDetail/ProductDetail.tsx
@@ -1,30 +1,32 @@
 import React, { FC, useEffect } from "react";
-import { useParams } from 'react-router-dom'
-import { useAppSelector, useAppDispatch } from '../../../Store/hooks'
-import { fetchProduct } from '../ProductCard/productCardSlice'
-import "./ProductDetail.scss"
-
+import { useParams } from "react-router-dom";
+import { useAppSelector, useAppDispatch } from "../../../Store/hooks";
+import { fetchProduct } from "../ProductCard/productCardSlice";
+import "./ProductDetail.scss";
 
 export const ProductDetail: FC = () => {
-
-    const product = useAppSelector(state => state.product.productData)
-  const loading = useAppSelector(state => state.product.loading)
-  const routeLinkText = `Products > ${product.category || loading} > ${product.name || loading}`
-  const dispatch = useAppDispatch()
-  const {id} = useParams();
+  const product = useAppSelector((state) => state.product.productData);
+  const loading = useAppSelector((state) => state.product.loading);
+  const routeLinkText = `Products > ${product.category || loading} > ${
+    product.name || loading
+  }`;
+  const dispatch = useAppDispatch();
+  const { id } = useParams();
   useEffect(() => {
-    console.log("Ravi")
+    console.log("Ravi");
     window.scrollTo(0, 0);
-    if(!product?._id || product._id !== id){
-      dispatch(fetchProduct(id||""))
+    if (!product?._id || product._id !== id) {
+      dispatch(fetchProduct(id || ""));
     }
-  }, [])
+  }, []);
 
-    
   return (
     <div className="pr0duct-detai1">
       <div className="route-link">{routeLinkText}</div>
-      <div className="product-detail-container"></div>
+      <div className="product-detail-container">
+        <div className="product-image"></div>
+        <div className="product-detail"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
