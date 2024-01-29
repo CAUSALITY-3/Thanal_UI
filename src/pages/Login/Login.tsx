734e48c9fc5ed41b048140151e4afbfb970a3c0b
CAUSALITY-3 <70097078+CAUSALITY-3@users.noreply.github.com>
2024-01-29 11:34:47 +0530

    sdf

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,32 +1,43 @@
-import { FC } from 'react';
+import { FC } from "react";
 import "./Login.scss";
-import { Button } from '../../components/Buttons/Button';
+import * as stylex from "@stylexjs/stylex";
+import { Button } from "../../components/Buttons/Button";
+import { useAppSelector } from "../../Store/hooks";
 
 const Login: FC = () => {
-    const vai = ''
+  const isMobile = useAppSelector((state) => state.nav.isMobile);
+  const styles = stylex.create({
+    loginOuterDiv: {
+        display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      width: "100%",
+    },
+    loginContainer: {
+      display: "flex",
+    //   alignItems: "center",
+    padding: "10px 20px",
+      flexDirection: "column",
+      background: "rgba(0, 0, 0, .03)",
+      width: "100%",
+      maxWidth: "500px",
+    },
+    steps: {
+        color: "grey",
+        marginBottom: "25px"
+    }
+  });
 
   return (
-      <>
-        <div className='joinpage-container'>
-            <div className='joinpage-center-div'>
-                <div className="joinpage-h1-div">
-                    <h1 className='joinpage-h1'>Sherlock Poker</h1>
-                </div>
-                <div className="joinpage-form">
-                    <div className="joinpage-form-content">
-                        <input type="text" placeholder='name' className='joinpage-form-name-room' required />
-                    </div>
-                    <div className="joinpage-form-content">
-                        <input type="text" placeholder='room' className='joinpage-form-name-room' disabled value={vai} />
-                    </div>
-                    <div className="joinpage-form-content">
-                        <Button content="JOIN NOW"  />
-                    </div>
-                </div>
-            </div>
+    <>
+      <div {...stylex.props(styles.loginOuterDiv)}>
+        <div {...stylex.props(styles.loginContainer)}>
+          <div {...stylex.props(styles.steps)}>Step 1 of 2</div>
+          
+          </div>
         </div>
-      </>
-  )
-}
+    </>
+  );
+};
 
-export default Login
+export default Login;
